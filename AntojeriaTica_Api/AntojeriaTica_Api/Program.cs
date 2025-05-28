22c597502907f6aaeae0155c4ef7b3730f8c4f52
Malpizarr <m.alpizar@hotmail.com>
2025-05-27 19:09:25 -0600

    Revert "error"
    This reverts commit 44925569d7ab97622f60e1cd462a6daee29061b6.

--- AntojeriaTica_Api/AntojeriaTica_Api/Program.cs
+++ AntojeriaTica_Api/AntojeriaTica_Api/Program.cs
@@ -7,9 +7,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = bui
-
-
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
