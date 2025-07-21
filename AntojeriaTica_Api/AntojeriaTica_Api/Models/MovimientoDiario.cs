dfc9d7c9e070440f86c50a943be54e7e5d823ce8
Yendry17 <ymora60855@ufide.ac.cr>
2025-07-20 18:20:28 -0600

    MDC-001 y MDC-002

---
+++ AntojeriaTica_Api/AntojeriaTica_Api/Models/MovimientoDiario.cs
@@ -0,0 +1,14 @@
+﻿namespace AntojeriaTica_Api.Models
+{
+    public class MovimientoDiario
+    {
+        public int IdMovimiento { get; set; }
+        public DateTime Fecha { get; set; }
+        public string TipoMovimiento { get; set; } = "";
+        public string Categoria { get; set; } = "";
+        public decimal Monto { get; set; }
+        public string Descripcion { get; set; } = "";
+        public int IdUsuario { get; set; }
+        public string NombreUsuario { get; set; } = "";
+    }
+}
